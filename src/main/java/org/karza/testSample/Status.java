2fef73b177ba9aec3fb7476adf361badcdfd1a39
aslam <rameezaslam9@gmail.com>
2016-12-13 21:39:48 +0530

    start up

---
+++ src/main/java/org/karza/testSample/Status.java
@@ -0,0 +1,7 @@
+package org.karza.testSample;
+
+public enum Status {
+	
+	PAYED,INVALID;
+
+}
---
+++ src/main/java/org/karza/testSample/entity/TaxPayer.java
@@ -0,0 +1,13 @@
+package org.karza.testSample.entity;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+@Data
+@Entity
+public class TaxPayer {
+	
+	private Long tin;
+	
+
+}
